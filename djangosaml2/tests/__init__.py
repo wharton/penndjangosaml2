28dd596767db858f190d37d4cf846567a604e144
Lorenzo Gil Sanchez <lgs@yaco.es>
2010-08-22 10:15:43 +0200

    Add test for wayf service

--- djangosaml2/tests/__init__.py
+++ djangosaml2/tests/__init__.py
@@ -35,7 +35,7 @@ class SSOTests(TestCase):
         self.assertEquals(remove_variable_attributes(xml1),
                           remove_variable_attributes(xml2))
 
-    def test_login(self):
+    def test_login_one_idp(self):
         # monkey patch SAML configuration
         views._load_conf = conf.create_conf(sp_host='sp.example.com',
                                             idp_hosts=['idp.example.com'])
@@ -55,5 +55,54 @@ class SSOTests(TestCase):
         saml_request = params['SAMLRequest'][0]
         expected_request = """<?xml version='1.0' encoding='UTF-8'?>
 <ns0:AuthnRequest AssertionConsumerServiceURL="http://sp.example.com/saml2/acs/" Destination="https://idp.example.com/simplesaml/saml2/idp/SSOService.php" ID="XXXXXXXXXXXXXXXXXXXXXX" IssueInstant="2010-01-01T00:00:00Z" ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" ProviderName="Test SP" Version="2.0" xmlns:ns0="urn:oasis:names:tc:SAML:2.0:protocol"><ns1:Issuer xmlns:ns1="urn:oasis:names:tc:SAML:2.0:assertion">http://sp.example.com/saml2/metadata/</ns1:Issuer><ns0:NameIDPolicy AllowCreate="true" Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient" /></ns0:AuthnRequest>"""
+        xml = decode_base64_and_inflate(saml_request)
+        self.assertSAMLRequestsEquals(expected_request, xml)
+
+        # if we set a next arg in the login view, it is preserverd
+        # in the RelayState argument
+        next = '/another-view/'
+        response = self.client.get('/login/', {'next': next})
+        self.assertEquals(response.status_code, 302)
+        location = response['Location']
+
+        url = urlparse.urlparse(location)
+        self.assertEquals(url.hostname, 'idp.example.com')
+        self.assertEquals(url.path, '/simplesaml/saml2/idp/SSOService.php')
+
+        params = urlparse.parse_qs(url.query)
+        self.assert_('SAMLRequest' in params)
+        self.assert_('RelayState' in params)
+        self.assertEquals(params['RelayState'][0], next)
+
+    def test_login_several_idps(self):
+        views._load_conf = conf.create_conf(sp_host='sp.example.com',
+                                            idp_hosts=['idp1.example.com',
+                                                       'idp2.example.com',
+                                                       'idp3.example.com'])
+        response = self.client.get('/login/')
+        self.assertContains(response, 'Where are you from?', status_code=200)
+        for i in range(1, 4):
+            link = '/login/?idp=https://idp%d.example.com/simplesaml/saml2/idp/metadata.php&next=/'
+            self.assertContains(response, link % i)
+
+        # click on the second idp
+        response = self.client.get('/login/', {
+                'idp': 'https://idp2.example.com/simplesaml/saml2/idp/metadata.php',
+                'next': '/',
+                })
+        self.assertEquals(response.status_code, 302)
+        location = response['Location']
+
+        url = urlparse.urlparse(location)
+        self.assertEquals(url.hostname, 'idp2.example.com')
+        self.assertEquals(url.path, '/simplesaml/saml2/idp/SSOService.php')
+
+        params = urlparse.parse_qs(url.query)
+        self.assert_('SAMLRequest' in params)
+        self.assert_('RelayState' in params)
+
+        saml_request = params['SAMLRequest'][0]
+        expected_request = """<?xml version='1.0' encoding='UTF-8'?>
+<ns0:AuthnRequest AssertionConsumerServiceURL="http://sp.example.com/saml2/acs/" Destination="https://idp2.example.com/simplesaml/saml2/idp/SSOService.php" ID="XXXXXXXXXXXXXXXXXXXXXX" IssueInstant="2010-01-01T00:00:00Z" ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" ProviderName="Test SP" Version="2.0" xmlns:ns0="urn:oasis:names:tc:SAML:2.0:protocol"><ns1:Issuer xmlns:ns1="urn:oasis:names:tc:SAML:2.0:assertion">http://sp.example.com/saml2/metadata/</ns1:Issuer><ns0:NameIDPolicy AllowCreate="true" Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient" /></ns0:AuthnRequest>"""
         xml = decode_base64_and_inflate(saml_request)
         self.assertSAMLRequestsEquals(expected_request, xml)
