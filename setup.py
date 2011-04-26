c93c5aa68084ede973f43b965dd353e7fa86c5ae
Lorenzo Gil Sanchez <lgs@yaco.es>
2011-04-26 11:24:10 +0200

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='djangosaml2',
-    version='0.1.2dev',
+    version='0.2.0',
     description='pysaml2 integration in Django',
     long_description='\n\n'.join([read('README'), read('CHANGES')]),
     classifiers=[
