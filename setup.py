a262aeda8b706848b33d30353a9f269daf3acb0d
Jozef <knaperek@users.noreply.github.com>
2015-06-05 14:15:20 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='djangosaml2',
-    version='0.13.0',
+    version='0.13.1',
     description='pysaml2 integration in Django',
     long_description='\n\n'.join([read('README'), read('CHANGES')]),
     classifiers=[
