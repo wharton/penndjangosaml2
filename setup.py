0498edad9b608aaebc3389fc8b44d5fa66c333b9
Jozef Knaperek <jknaperek@gmail.com>
2017-04-11 13:20:04 +0200

    Upgrade pysaml2 dependency to latest v4.4.0
    This could finally fix issues with serialization

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'pysaml2==4.0.5',
+        'pysaml2==4.4.0',
         'defusedxml==0.4.1'
         ],
     )
