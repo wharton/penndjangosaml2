68babeba0413f9d5c14b6fab2822300b10ff424e
Stephen Turoscy <sturoscy@gmail.com>
2017-05-03 17:26:18 -0400

    LOGGING!!!

--- penndjangosaml2/utils.py
+++ penndjangosaml2/utils.py
@@ -20,13 +20,13 @@ from . import settings as saml_settings
 
 import logging, requests
 
-logger = logging.getLogger()
-hdlr = logging.FileHandler('/logs/penndjangosaml2.log')
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.setLevel(logging.WARNING)
-logger.error(saml_settings.INCLUDE_PENN_GROUPS)
+# logger = logging.getLogger()
+# hdlr = logging.FileHandler('/logs/penndjangosaml2.log')
+# formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+# hdlr.setFormatter(formatter)
+# logger.addHandler(hdlr)
+# logger.setLevel(logging.WARNING)
+# logger.error(saml_settings.INCLUDE_PENN_GROUPS)
 
 
 def build_user_groups(user):
